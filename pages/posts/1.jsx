3a96943b1209121dc801f2091bf0b809996ca700
thien115116 <tranngocthien12a1@gmail.com>
2021-10-07 15:22:31 +0700

    Fix bug

--- pages/posts/1.jsx
+++ pages/posts/1.jsx
@@ -1,24 +1,4 @@
 import React from "react";
-import { useParams } from "react-router";
-
 export default function Post1() {
-  const { params } = useParams();
-  console.log(params);
   return <div>This is Post 1</div>;
-}
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const res = await fetch(
-    "https://api.apify.com/v2/key-value-stores/QubTry45OOCkTyohU/records/LATEST"
-  );
-  const posts = await res.json();
-
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      posts,
-    },
-  };
 }
